6acabeb05bdda7f9ed98234dd7dea3161a6787b4
Arifubila <arifubila5789@gmail.com>
2025-07-27 18:16:37 +0700

    Feat: Create Footer

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+// src/components/Footer.jsx
+export default function Footer() {
+  return (
+    <footer className="bg-blue-800 text-white text-center py-4">
+      <p>© 2025 SMKN 4 Tasikmalaya. All rights reserved.</p>
+    </footer>
+  );
+}
